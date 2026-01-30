04a5c91b57049fd9651989d55058e92760a5366b
parapati chennakesavarao <parapatichennakesavarao@gmail.com>
2026-01-30 15:03:46 +0530

    Random Operations using Import random

---
+++ RandomOperations.py
@@ -0,0 +1,13 @@
+import random
+#dice=random.randint(1, 6)
+#randomly generating a number between 1 and 100
+"""dice = random.randint(1, 100)  # generates a random integer between 1 and 100
+print("You rolled a", dice)#prints a random integer between 1 and 100"""
+
+#Randomly selecting a student from a list
+"""StudentNames=["Chenna","Abhi","Charan","himanth","Jasmith"]
+selected_student=random.choice(StudentNames)#randomly selects a name from the list
+print("Selected Student:",selected_student)"""
+
+Otp_generator=random.randint(1000,9999)
+print("Generated OTP:",Otp_generator)
