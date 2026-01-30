95dafeed663ee192140558096234fcbbbb3f3890
parapati chennakesavarao <parapatichennakesavarao@gmail.com>
2026-01-30 10:56:11 +0530

    Math Operations by importing math

---
+++ MathOperations.py
@@ -0,0 +1,10 @@
+import math
+num=16
+print("The square root of", num, "is", math.sqrt(num))#square root function, prints 4.0
+print(math.factorial(5))#factorial function, prints 120
+print(math.pow(2,3))#power function, prints 8.0
+nums=7.8
+print(math.floor(nums))#prints 7
+print(math.ceil(nums))#prints 8
+print(math.fabs(-10))#prints 10.0
+print(math.gcd(48,18))#greatest common divisor, prints 6
